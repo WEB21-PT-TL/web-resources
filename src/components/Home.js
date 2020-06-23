fd2c4039941de010a2651c916e7a75be8c6a7ea3
Aja Blanco <aja@Ajas-MacBook-Pro.local>
2020-06-22 21:18:24 -0700

    add ant design navbar template

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,66 @@
 import React from "react";
+import "antd/dist/antd.css";
+import { Layout, Menu } from 'antd';
+import {
+  HomeOutlined,
+  VideoCameraOutlined,
+  PaperClipOutlined,
+  SnippetsOutlined,
+  ExclamationOutlined,
+  QuestionOutlined,
+  LinkOutlined,
+  CheckOutlined
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 export default function Home() {
-  return <div>Testing</div>;
-}
+  return (
+  <Layout>
+    <Sider
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+      }}
+    >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu.Item key="1" icon={<HomeOutlined />}>
+          Home
+        </Menu.Item>
+        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+          Videos
+        </Menu.Item>
+        <Menu.Item key="3" icon={<PaperClipOutlined />}>
+          Docs
+        </Menu.Item>
+        <Menu.Item key="4" icon={<SnippetsOutlined />}>
+          Material
+        </Menu.Item>
+        <Menu.Item key="5" icon={<ExclamationOutlined />}>
+          Important
+        </Menu.Item>
+        <Menu.Item key="6" icon={<QuestionOutlined />}>
+          FAQ
+        </Menu.Item>
+        <Menu.Item key="7" icon={<LinkOutlined />}>
+          Links
+        </Menu.Item>
+        <Menu.Item key="8" icon={<CheckOutlined />}>
+          Interview Prep
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout className="site-layout" style={{ marginLeft: 200 }}>
+      <Header className="site-layout-background" style={{ padding: 0 }} />
+      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+        <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
+
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>
+  )};
