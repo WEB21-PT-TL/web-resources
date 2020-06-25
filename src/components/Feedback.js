f4386e70f73cd302b2f1a4831c7af33f418c7209
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-25 16:33:45 -0400

    Backend Done, admin page done, form submit done

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Input, Button, Select } from "antd";
 import "antd/dist/antd.css";
+import Axios from "axios";
 
 export default function Feedback() {
+  const [formState, setForm] = useState({
+    email: "",
+    message: "",
+    reason: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    Axios.post(
+      "https://web-resources-api.herokuapp.com/api/submissions",
+      formState
+    )
+      .then((res) => {
+        alert("Form submitted!");
+      })
+      .catch((err) => {
+        alert(
+          "Something went wrong, we could not submit your form, try again later."
+        );
+      });
+  };
+
   const layout = {
     labelCol: { span: 7 },
     wrapperCol: { span: 10 },
@@ -12,8 +41,9 @@ export default function Feedback() {
       <h2> Do you have any Feedback? </h2>
       <h5>
         Feel free to suggest any resources you have found helpful that are not
-        listed here, <br/>topics you think we should add or expand on, or simply leave
-        a kind message. <br/> Any and all feedback is welcome.{" "}
+        listed here, <br />
+        topics you think we should add or expand on, or simply leave a kind
+        message. <br /> Any and all feedback is welcome.{" "}
       </h5>
       <br />
 
@@ -21,35 +51,48 @@ export default function Feedback() {
         <Form.Item
           {...layout}
           label="Email"
-          name="Email"
+          onChange={handleChange}
           rules={[{ required: true, message: "Please input your email!" }]}
         >
-          <Input />
+          <Input name="email" value={formState.email} />
         </Form.Item>
         <Form.Item label="Select" {...layout}>
-          <Select>
-            <Select.Option value="other">
+          <Select
+            value={formState.reason}
+            onChange={(e) => setForm({ ...formState, reason: e })}
+          >
+            <Select.Option value="I'd like to Suggest a Resource">
               I'd like to Suggest a Resource
             </Select.Option>
-            <Select.Option value="other">Please add a topic</Select.Option>
-            <Select.Option value="other">Mistake/Misleading Info</Select.Option>
-            <Select.Option value="other">Thank you!</Select.Option>
-            <Select.Option value="other">Other</Select.Option>
+            <Select.Option value="Please add a topic">
+              Please add a topic
+            </Select.Option>
+            <Select.Option value="Mistake/Misleading Info">
+              Mistake/Misleading Info
+            </Select.Option>
+            <Select.Option value="Thank you!">Thank you!</Select.Option>
+            <Select.Option value="Other">Other</Select.Option>
           </Select>
         </Form.Item>
         <Form.Item
           {...layout}
           label="Message"
-          name="Message"
+          onChange={handleChange}
           rules={[{ required: true, message: "Please leave a message" }]}
         >
-          <Input.TextArea />
+          <Input.TextArea name="message" value={formState.message} />
         </Form.Item>
-        <Button id="feedback">Submit</Button>
+        <Button id="feedback" onClick={handleSubmit}>
+          Submit
+        </Button>
       </Form>
-      <br/>
-      <br/>
-      <h6>Brought to you by your favorite TL's: <br/>Aja Blanco, Nathaniel Richards, Songa Mugenzi, James Coulter, <br/>Eddie Blanciak, Latosha Tims, Jay Ren, and Marc Tapp.</h6>
+      <br />
+      <br />
+      <h6>
+        Brought to you by your favorite TL's: <br />
+        Aja Blanco, Nathaniel Richards, Songa Mugenzi, James Coulter, <br />
+        Eddie Blanciak, Latosha Tims, Jay Ren, and Marc Tapp.
+      </h6>
     </div>
   );
 }
